a6d689513b42e7692d35d35c6d1fa0bfea976dc3
saeid-rasoli <saeidrasoli1212@gmail.com>
2022-02-24 12:45:50 +0330

    templates

--- moshavere/urls.py
+++ moshavere/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'moshavere'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup')
 ]
--- moshavere/views.py
+++ moshavere/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html', {})
+
+def signup(request):
+    return render(request, 'auth/signup.html', {})
+
+def login(request):
+    return render(request, 'auth/login.html', {})
